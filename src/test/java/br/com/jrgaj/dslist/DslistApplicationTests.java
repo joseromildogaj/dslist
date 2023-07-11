f74b8cb55bf39ea392524b371ba20c496e000314
josé Romildo <pronuno1@gmail.com>
2023-07-10 22:03:07 -0300

    Project Created

---
+++ src/test/java/br/com/jrgaj/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jrgaj.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
